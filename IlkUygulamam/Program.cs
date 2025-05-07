4e3171ef57d7734670ac4f99e32a7a0143cd1686
rabiaduman <rbiaduman@gmail.com>
2025-05-07 13:45:44 +0300

    Add project files.

---
+++ IlkUygulamam/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+string ad = "Rabia", soyad = "Duman", neden = "Yazilim ogrenmek ve bu alanda calismak icin Patika Bootcampteyim"; //Degiskenlerin tanimlanmasi ve atama yapilmasi
+
+
+Console.WriteLine(ad + " " + soyad); //Ekrana yazdirma
+Console.WriteLine(neden); //Ekrana yazdirma
