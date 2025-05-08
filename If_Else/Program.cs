d5d234950466f5cb933ab8b5e4a99bfb204acff5
rabiaduman <rbiaduman@gmail.com>
2025-05-08 19:19:50 +0300

    Pratik - If / Else eklendi

---
+++ If_Else/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.Write("Lütfen bir sayi giriniz: ");
+
+int sayi = Convert.ToInt32(Console.ReadLine()); //Girilen metinsel ifade  tam sayıya cevriliyor.
+
+if (sayi == 10)
+{
+    Console.WriteLine("Girilen sayi 10'a esittir.");
+    
+}
+else if (sayi < 10)
+{
+    Console.WriteLine("Girilen sayi 10'dan kücüktür.");
+
+}
+else
+{
+    Console.WriteLine("Girilen sayi 10'dan büyüktür.");
+}
+
+if (sayi % 2 == 0) //Tek mi cift mi anlamak için sayinin 2 ile bolumunden kalanin 0 a esit olma durumuna bakiliyor. 
+{
+    Console.WriteLine("Girilen sayi cifttir.");
+}
+else
+{
+    Console.WriteLine("Girilen sayi tektir.");
+}
